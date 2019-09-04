c86f3b02e51e8ef0abada801058810d20bd2c690
xCommunicado <xCommunicado@EOSVM>
2019-09-04 12:56:31 +0600

    manual test developing

---
+++ manualtest.sh
@@ -0,0 +1,26 @@
+# Default Search Test
+
+searchit Default Search
+
+#  Search Engine Test
+
+#Duckduckgo
+echo "DuckDuckGo Search Test"
+searchit -ddg Default Duckduckgo Search
+searchit -duckduckgo Default Duckduckgo Search
+searchit -ddg -img Duckduckgo Image Search
+searchit -ddg -image Duckduckgo Image Search II
+searchit -ddg -vid Duckduckgo Video Search
+searchit -ddg -video Duckduckgo Video Search II
+searchit -ddg -nws Duckduckgo News Search
+searchit -ddg -news DuckDuckGo News Search II
+searchit -ddg -mns Duckduckgo meanings search
+searchit -ddg -meanings DuckDuckGo meanings search II
+searchit -ddg -ans DuckDuckGo Answer Search
+searchit -ddg -answer DuckDuckGo answer search II
+searchit -ddg -ntr DuckDuckGo Nutrition Search 
+searchit -ddg -nutrition DuckDuckGo Nutrition Search II
+searchit -ddg -rcp DuckDuckGo recipes search
+searchit -ddg -recipes DuckDuckGo recipes Search II
+clear
+echo "DuckDuckGo Search Test Done"
