2c154805b2f832b79ff20e6dc972f14f34cf4857
xCommunicado <xCommunicado@EOSVM>
2019-09-04 13:54:16 +0600

    google added

--- manualtest.sh
+++ manualtest.sh
@@ -1,13 +1,18 @@
+#!/usr/bin/env bash
+
 # Default Search Test
 
 searchit Default Search
 
 #  Search Engine Test
 
-#Duckduckgo
+#Duckduckgo 
+
+echo "Start DuckDuckGo Search Test? ('Ctrl+C' to cancel)"
+read x
 echo "DuckDuckGo Search Test"
 searchit -ddg Default Duckduckgo Search
-searchit -duckduckgo Default Duckduckgo Search
+searchit -duckduckgo Default Duckduckgo Search II
 searchit -ddg -img Duckduckgo Image Search
 searchit -ddg -image Duckduckgo Image Search II
 searchit -ddg -vid Duckduckgo Video Search
@@ -24,3 +29,25 @@ searchit -ddg -rcp DuckDuckGo recipes search
 searchit -ddg -recipes DuckDuckGo recipes Search II
 clear
 echo "DuckDuckGo Search Test Done"
+
+#Google 
+
+echo "Start Google Search Test? ('Ctrl+C' to cancel)"
+read x
+echo "Google Search Test"
+searchit -ggl Default Google Search
+searchit -google Defaul Google Search II
+searchit -ggl -img Google Image Search
+searchit -ggl -image Google Image Search II
+searchit -ggl -vid Google Video Search
+searchit -ggl -video Google Video Search II
+searchit -ggl -nws Gooogle News Search
+searchit -ggl -news Google News Search II
+searchit -ggl -bks Google Book Search
+searchit -ggl -books Google Book Search II
+searchit -ggl -fnc Google Finance Search
+searchit -ggl -finance google Finance Search II
+searchit -ggl -map Google Map Search
+searchit -ggl -maps Google Map Search II
+clear
+echo "Google Search Test Done"
