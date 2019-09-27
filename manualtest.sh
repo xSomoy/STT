f4811d0ee7af1f8c90db08eb9039a1bc9c41f44f
xSomoy <xsomoy@tutanota.com>
2019-09-27 14:55:13 +0600

    typo mistake in qwant association search

--- manualtest.sh
+++ manualtest.sh
@@ -935,7 +935,7 @@ echo "Test Done ( $testDone / $totalTest )"
 searchit -qwnt -asn Qwant Asssocian Search
 ((testDone++))
 echo "Test Done ( $testDone / $totalTest )"
-searchit -qwnt -associations Qwant Asssocian Search II
+searchit -qwnt -association Qwant Asssocian Search II
 ((testDone++))
 echo "Test Done ( $testDone / $totalTest )"
 clear
